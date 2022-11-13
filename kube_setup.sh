16aeeaa5411c4c30a3395a1c9ccc72bf65891d41
Jack Dragos <jack.dragos@gmail.com>
2022-11-13 17:05:11 -0600

    FIX: remove ingress

--- kube_setup.sh
+++ kube_setup.sh
@@ -2,8 +2,3 @@ kubectl apply -f sveltekit_frontend/kube/deployment.yaml
 kubectl apply -f sveltekit_frontend/kube/service.yaml
 kubectl apply -f python_backend/kube/deployment.yaml
 kubectl apply -f python_backend/kube/service.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
-kubectl apply -f dashboard_adminuser.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.4.0/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f ingress.yaml
-kubectl port-forward --namespace=ingress-nginx service/ingress-nginx-controller 80:80
